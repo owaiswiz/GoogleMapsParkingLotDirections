c5421786b7bc8a28b156daa933571b745b35d665
Owais <owaiswiz@gmail.com>
2017-12-21 14:11:27 +0530

    Create Main App Component
    * Entry Point

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+import React, { Component } from 'react';
+
+import ParkingLot from './ParkingLot/ParkingLot';
+import logo from './logo.svg';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <ParkingLot googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCkUOdZ5y7hMm0yrcCQoCvLwzdM6M8s5qk&v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
